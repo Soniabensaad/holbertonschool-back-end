4b0767b2472cb518311229f5a7acb10181d8e3de
Soniabensaad <5687@holbertonstudents.com>
2023-04-11 18:20:39 +0200

    hard to do

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,15 +13,13 @@ if __name__ == "__main__":
       about his/her TODO list progress."""
     api_url = "https://jsonplaceholder.typicode.com/"
     employee_ID = sys.argv[1]
-    user = requests.get(api_url + "users/{}".format(employee_ID))
-    user_data = user.json()
-    employee_name = user_data['name']
-    todos = requests.get(api_url + "todos", params={"userId": employee_ID})
-    todos_data = todos.json()
-    total_tasks = len(todos_data)
-    tasks_completed = sum(1 for task in todos_data if task['completed'])
-    print("Employee {} is done with tasks ({}/{})".
-          format(user_data.get("name"), tasks_completed, total_tasks))
-    for task in todos_data:
-       if task['completed'] is True:
-        print("\t{}   {}".format(task['title'], task['completed']))
+    user = requests.get(api_url + "users/{}".format(employee_ID)).json()
+    todos = requests.get(api_url + "todos", params={"userId": employee_ID}).json()
+    complete = []
+    for task in todos:
+        if task.get("completed") is True:
+            complete.append(task.get("title"))
+    print("Employee {} is done with tasks({}/{})".
+          format(task.get("name"), len(complete), len(todos)))
+    for i in complete:
+        print("\t {}".format(complete))
