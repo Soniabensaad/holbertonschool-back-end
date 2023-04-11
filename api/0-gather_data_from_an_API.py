0698e1c8194a1425991091396c5b87ad1b241ef5
Soniabensaad <5687@holbertonstudents.com>
2023-04-11 19:46:44 +0200

    hard to do

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,31 +6,26 @@ his/her TODO list progress.
 """
 import requests
 import sys
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-# check if the correct number of arguments is provided
-if len(sys.argv) != 2:
-    sys.exit(1)
+    employee_id = sys.argv[1]
 
-# get the employee ID from the command line argument
-employee_id = sys.argv[1]
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
 
-# make the API request
-response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
 
-# check if the API request was successful
-if response.status_code != 200:
-    sys.exit(1)
+    if response.status_code != 200:
+        sys.exit(1)
+  
 
-# parse the API response
-tasks = response.json()
+    tasks = response.json()
 
-# calculate the progress of the employee's TODO list
-total_tasks = len(tasks)
-done_tasks = sum(task["completed"] for task in tasks)
-employee_name = tasks[0]["username"]
+    total_tasks = len(tasks)
+    done_tasks = sum(task["completed"] for task in tasks)
+    employee_name = tasks[0]["username"]
 
-# display the progress of the employee's TODO list
-print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
-for task in tasks:
-    if task["completed"]:
-        print(f"\t {task['title']}")
+    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
+    for task in tasks:
+       if task["completed"]:
+           print(f"\t {task['title']}")
