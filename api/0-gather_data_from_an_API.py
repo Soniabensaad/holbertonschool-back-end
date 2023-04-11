4b187f565d934d7f073e1071311b0f88f4f6fb0c
Soniabensaad <5687@holbertonstudents.com>
2023-04-11 17:48:55 +0200

    hard to do

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """returns information about his/her TODO list progress."""
-import requests
-import json
 import sys
+import requests
 
 api_url = "https://jsonplaceholder.typicode.com/"
 employee_ID = sys.argv[1]
 user = requests.get(api_url + "users/{}".format(employee_ID))
 user_data = user.json()
 employee_name = user_data['name']
-todos = requests.get(api_url + "todos", params={"userId: employee_ID"})
+todos = requests.get(api_url + "todos", params={"userId": employee_ID})
 todos_data = todos.json()
 total_tasks = len(todos_data)
-tasks_completed = sum (1 for task in total_tasks if task['completed'])
-print("Employee{} is done with tasks({}/{}):"format.(employee_name, tasks_completed, total_tasks))
-for task in total_tasks:
+tasks_completed = sum(1 for task in todos_data if task['completed'])
+print("Employee {} is done with tasks ({}/{})".format(employee_name, tasks_completed, total_tasks))
+for task in todos_data:
     if task['completed']:
-         print("\t{}   {}".format(task['title'], task['completed']))
-
+        print("\t{}   {}".format(task['title'], task['completed']))
