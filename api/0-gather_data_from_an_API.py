d94a56e6cdb4f69aa40a0e09099f5ff1d9247f4c
Soniabensaad <5687@holbertonstudents.com>
2023-04-11 19:14:47 +0200

    hard to do

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,26 +4,30 @@ Write a Python script that, using this REST API,
 for a given employee ID, returns information about
 his/her TODO list progress.
 """
-import requests
-import sys
+if __name__ == '__main__':
+    """TODO list progress."""
+    import requests
+    import sys
 
-id = sys.argv[1]
-response = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + id)
-todos = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId=' + id)
+    id = sys.argv[1]
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + id)
+    todos = requests.get(
+         'https://jsonplaceholder.typicode.com/todos?userId=' + id)
 
-employee_file = response.json()
-todos_file = todos.json()
+    employee_file = response.json()
+    todos_file = todos.json()
 
-employee_name = employee_file['name']
-total_tasks = len(todos_file)
-completed = sum(1 for todo in todos_file if todo['completed'])
-titles = [todo['title']
-            for todo in todos_file if todo['completed']]
+    employee_name = employee_file['name']
+    total_tasks = len(todos_file)
+    completed = sum(1 for todo in todos_file if todo['completed'])
+    titles = [todo['title']
+             for todo in todos_file if todo['completed']]
 
-phrase = "is done with tasks"
-print(
-    "Employee {} {}({}/{}):".format(employee_name, phrase, completed, total_tasks))
-for title in titles:
-    print("\t {}".format(title))
+    phrase = "is done with tasks"
+    print(
+        "Employee {} {}({}/{}):".format(employee_name, phrase, completed, total_tasks))
+    for title in titles:
+        print("\t {}".format(title))
+
+  
