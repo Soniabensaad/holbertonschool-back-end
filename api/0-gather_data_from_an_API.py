d0190ba8998ff2cb8d776e143d00191b52d12736
Soniabensaad <5687@holbertonstudents.com>
2023-04-11 18:24:41 +0200

    hard to do

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,15 +11,17 @@ if __name__ == "__main__":
     """Write a Python script that, using this REST API,
       for a given employee ID, returns information
       about his/her TODO list progress."""
+    
     api_url = "https://jsonplaceholder.typicode.com/"
-    employee_ID = sys.argv[1]
-    user = requests.get(api_url + "users/{}".format(employee_ID)).json()
-    todos = requests.get(api_url + "todos", params={"userId": employee_ID}).json()
+    user = requests.get(api_url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(
+        api_url + "todos", params={"userId": sys.argv[1]}).json()
+
     complete = []
     for task in todos:
         if task.get("completed") is True:
             complete.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{})".
-          format(task.get("name"), len(complete), len(todos)))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(complete), len(todos)))
     for i in complete:
-        print("\t {}".format(complete))
+        print("\t {}".format(i))
