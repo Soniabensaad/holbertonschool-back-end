472f6f8a3bed4db75fe3f3a681aef82832debbf6
Soniabensaad <5687@holbertonstudents.com>
2023-04-12 15:22:14 +0200

    hard to commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,20 +7,28 @@ Format must be:
 "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 File name must be: USER_ID.csv
 """
-import csv
-import requests
-import sys
 
 if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/" + user_id
-    res = requests.get(url_user).json()
-    username = res.get("username")
-    req = requests.get(
-        "https://jsonplaceholder.typicode.com/users/" +
-        (user_id) + '/todos')
-    with open("{}.csv".format(sys.argv[1]), "w") as file_c:
-        writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
-        for task in req.json():
-            writer.writerow([user_id, username,
-                            task.get("completed"), task.get("title")])
+    import csv
+    import requests
+    import sys
+
+    t = sys.argv[1]
+    response = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + t)
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + t)
+
+    employee = response.json()
+    todos = todos.json()
+    id = employee['id']
+    name = employee['username']
+
+    with open(f'{t}.csv', 'w') as file:
+        for i in todos:
+            Ta = i['completed']
+            TASK_TITLE = i['title']
+            TASK_COMPLETED_STATUS = i['completed']
+            TASK_TITLE = i['title']
+            file.write(
+                f"\"{id}\",\"{name}\",\"{Ta}\",\"{TASK_TITLE}\"\n")
