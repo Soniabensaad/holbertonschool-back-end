34c11100e55d13b523433ae9965237fbfbac0e2f
Soniabensaad <5687@holbertonstudents.com>
2023-04-12 14:11:23 +0200

    hard to work

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,3 +7,19 @@ Format must be:
 "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 File name must be: USER_ID.csv
 """
+import csv
+import requests
+import sys
+if __name__ == '__main__':
+    id = sys.argv[1]
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id
+    user_data = requests.get(url_user).json()
+    username = user_data.get('username')
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' +
+        (id) + '/todos')
+    with open("{}.csv".format(id), "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in tasks.json():
+            writer.writerow([id, username,
+                            task.get("completed"), task.get("title")])
