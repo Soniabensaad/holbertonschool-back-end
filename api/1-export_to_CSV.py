2c101c7327343730f45d78a42de8f5d6024dda7e
Soniabensaad <5687@holbertonstudents.com>
2023-04-12 14:05:18 +0200

    hard to work

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0, extend your Python script 
+to export data in the CSV format.
+Records all tasks that are owned by this employee
+Format must be: 
+"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
