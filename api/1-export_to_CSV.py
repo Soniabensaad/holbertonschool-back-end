9f79011aa00a7066dcaab2767fec7cdd63ef9e85
Soniabensaad <5687@holbertonstudents.com>
2023-04-12 15:13:52 +0200

    hard to commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,15 +12,15 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    id_c = sys.argv[1]
+    user_id = sys.argv[1]
     url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
     res = requests.get(url_user).json()
     username = res.get("username")
     req = requests.get(
         'https://jsonplaceholder.typicode.com/users/' +
-        (id_c) + '/todos')
+        (user_id) + '/todos')
     with open("{}.csv".format(sys.argv[1]), "w") as file_c:
         writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
         for task in req.json():
-            writer.writerow([id_c, username,
+            writer.writerow([user_id, username,
                             task.get("completed"), task.get("title")])
