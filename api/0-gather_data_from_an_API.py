5938f4580f03261fd487de7da04d171521b49f2f
Soniabensaad <5687@holbertonstudents.com>
2023-04-12 14:22:53 +0200

    hard to work

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""returns information about his/her TODO list progress."""
-import requests
-import sys
 """
 Write a Python script that, using this REST API,
 for a given employee ID, returns information about
